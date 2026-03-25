c3695b95c2818bb74c2e18d649e7124caa477d7d
2Kelvin <rocketmancodes02@gmail.com>
2026-03-25 07:39:43 +0300

    update hello world printed message

--- app.py
+++ app.py
@@ -4,10 +4,7 @@ app = Flask(__name__)
 
 @app.route('/')
 def hello_world():
-    return jsonify({
-        "message": "Hello from the containerized Flask API!",
-        "status": "success"
-    })
+    return 'Hello from the containerized Flask API!'
 
 if __name__ == '__main__':
     # used 0.0.0.0 so it's accessible outside the container
